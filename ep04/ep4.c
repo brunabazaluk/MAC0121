5b6b74e96c1864496037b1da72df9d00db2f3c97
brunabazaluk <bruna@opal.localdomain>
2018-11-24 22:14:49 +0000

    updates
    Signed-off-by: brunabazaluk <bruna@opal.localdomain>

--- ep04/ep4.c
+++ ep04/ep4.c
@@ -2,11 +2,13 @@
 #include <stdlib.h>
 #include <string.h>
 
+int busca_bin(char * palavra, char **v, int * v_indices, int n);
 void lista_desordenada(FILE * texto);
 void v_ordenado(FILE * texto);
 int busca_vd(char * palavra, char ** v, int n);
 char * le_palavra(FILE * texto);
 void v_desordenado(FILE * texto);
+void lista_ordenada(FILE * texto);
 
 typedef struct elo{
 	
@@ -33,9 +35,65 @@ int main(int argc, char ** argv)
 	else if(strcmp(argv[2],"LD")==0)
 		lista_desordenada(filetxt);
 
+	else if(strcmp(argv[2],"VO")==0)
+		v_ordenado(filetxt);
+
+	else if(strcmp(argv[2],"LO")==0)
+		lista_ordenada(filetxt);
+
+
 	return(0);
 
 }
+
+void lista_ordenada(FILE * texto)
+{
+	char * palavra;
+	Elo comeco, lista, novo;
+
+	comeco = malloc(sizeof(Elo));
+	comeco->palavra=malloc(30*sizeof(char));
+	comeco->prox=NULL;
+	comeco->quantidade=1;
+	
+	palavra=le_palavra(texto);
+	
+	while(palavra!=NULL)
+	{
+		lista=comeco;
+		while(lista->prox!=NULL && strcmp(lista->palavra,palavra)!=0)
+		{
+			lista=lista->prox;
+		}
+		
+		if(strcmp(lista->palavra,palavra)==0)
+		{
+			lista=lista->prox;
+			lista->quantidade+=1;
+		}
+		else if(lista->prox==NULL)
+		{
+			novo = malloc(sizeof(Elo));
+			novo->palavra=malloc(30*sizeof(char));
+			novo->palavra=palavra;
+			novo->quantidade=1;
+			novo->prox=NULL;
+			lista->prox=novo;
+		}
+		palavra=le_palavra(texto);
+
+	}
+	
+	lista=comeco;
+
+	while(lista->prox!=NULL)
+	{
+		printf("%s %d\n", lista->palavra, lista->quantidade);
+		lista=lista->prox;
+	}
+
+}
+
 
 void lista_desordenada(FILE * texto)
 {
@@ -45,33 +103,32 @@ void lista_desordenada(FILE * texto)
 	comeco = malloc(sizeof(Elo));
 	comeco->palavra=malloc(30*sizeof(char));
 	comeco->prox=NULL;
+	comeco->quantidade=1;
 	
 	palavra=le_palavra(texto);
 	
 	while(palavra!=NULL)
 	{
 		lista=comeco;
-		printf("while palavra nao nula\n");
-		while(lista->prox!=NULL && strcmp(lista->prox->palavra,palavra)!=0)
+		while(lista->prox!=NULL && strcmp(lista->palavra,palavra)!=0)
 		{
-			printf("percorre ate achar ou acabar\n");
 			lista=lista->prox;
 		}
 		
-		if(lista->prox->palavra==palavra)
+		if(strcmp(lista->palavra,palavra)==0)
 		{
 			lista=lista->prox;
 			lista->quantidade+=1;
 		}
 		else if(lista->prox==NULL)
 		{
-		8	
 			novo = malloc(sizeof(Elo));
 			novo->palavra=malloc(30*sizeof(char));
+			novo->palavra=palavra;
+			novo->quantidade=1;
 			novo->prox=NULL;
 			lista->prox=novo;
 		}
-
 		palavra=le_palavra(texto);
 
 	}
@@ -90,7 +147,45 @@ void v_ordenado(FILE * texto)
 {	
 	char *palavra, **v_chave;
 	int *v_valor, busca;
-	int i=0,k, j, tamanho=0, fim, n=100;
+	int i=0,k, j=0, tamanho=0, fim, n=100;
+
+	v_chave = malloc(n*sizeof(char*));
+	v_valor = malloc(n*sizeof(int));
+	for(k=0;k<n;k++)
+	{
+		v_chave[k]=malloc(30*sizeof(char));
+	}
+		
+	palavra = le_palavra(texto);
+	while (palavra != NULL)
+	{	
+		j++;
+		printf("%d",j);
+		if(i>=n)
+		{
+			n=2*n;
+			v_chave=realloc(v_chave, n*sizeof(char *));
+			v_valor=realloc(v_valor, n*sizeof(int));
+		}
+		busca = busca_bin(palavra,v_chave,v_valor,n);
+		if(busca==-1)
+		{	
+			i++;
+		}
+		else
+		{
+			v_valor[busca]+=1;
+		}
+	
+		palavra=le_palavra(texto);
+	}
+
+	for(k=0;k<i;k++)
+	{
+		printf("%s %d\n", v_chave[k], v_valor[k]);
+	}
+
+
 }
 
 void v_desordenado(FILE * texto)
@@ -166,4 +261,32 @@ int busca_vd(char * palavra, char ** v, int n)
 	return(-1);
 }
 
+int busca_bin(char * palavra, char **v, int * v_indices, int n)
+{
+	int k,esquerda=-1, direita=n, meio;
 
+	while(esquerda<direita-1)
+	{
+		meio=(esquerda+direita)/2;
+		if (strcmp(v[meio],palavra)<0)
+			esquerda = meio;
+		else direita = meio;
+	}
+	if (direita!=n && strcmp(palavra, v[direita])==0) 
+	{
+		printf("palavra igual\n");
+		return(direita);
+	}
+	else 
+	{
+		for(k=n;k>=direita;k--)
+		{
+			v[k]=v[k-1];
+			v_indices[k]=v_indices[k-1];
+		}
+		n++;
+		v[direita-1]=palavra;
+		return(-1);
+	}
+
+}
